2dff775dfc0df6448d93441c5e798277917d0b26
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-12-15 18:59:44 +0000

    search function imprived, fetching books
    single book component needs to be ammended to render on search, temp fix of adding directly to the parent component

--- src/Components/SpecialReleases.jsx
+++ src/Components/SpecialReleases.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Row, Dropdown, Container, InputGroup, FormControl, DropdownButton } from "react-bootstrap";
+import { Card, Col, Button } from "react-bootstrap";
+import MyBadge from "./MyBadge.jsx";
+import {
+  Row,
+  Dropdown,
+  Container,
+  InputGroup,
+  FormControl,
+  DropdownButton,
+} from "react-bootstrap";
 import Fantasy from "../Data/fantasy.json";
 import Horror from "../Data/horror.json";
 import History from "../Data/history.json";
 import Romance from "../Data/romance.json";
 import Scifi from "../Data/scifi.json";
-import SingleBook from "./SingleBook"
-import {fetchBooks} from "../utils"
-
+import SingleBook from "./SingleBook";
+import { fetchBooks } from "../utils";
 
 let bookCategories = ["fantasy", "horror", "history", "romance", "scifi"];
 let books = {
@@ -24,76 +32,96 @@ class SpecialReleases extends React.Component {
     categorySelected: "fantasy",
   };
 
+  selectBook = e => {
+    const element = e.currentTarget;
+    element.classList.toggle("selectedCard");
+  };
   componentDidMount = async () => {
-const books =  await fetchBooks()
-this.setState({books:books})
-  }
-
-
-  handleDropdownChange = (category) => {
-    this.setState({
-      books: books[category],
-      categorySelected: category,
-    });
+    const books = await fetchBooks();
+    this.setState({ books: books, allBooks: books });
   };
 
-  handleSearchQuery = (searchQuery) => {
+  handleDropdownChange = category => {
+    let bookCategory = this.state.allBooks.filter(
+      book => book.category === category
+    );
+    this.setState({ books: bookCategory, categorySelected: category });
+    console.log("books", this.state.books);
+    console.log("category", this.state.categorySelected);
+  };
+
+  handleSearchQuery = searchQuery => {
     if (searchQuery) {
-        console.log(searchQuery)
-      let filteredBooks = books.filter((book) =>
+      console.log(searchQuery);
+      let filteredBooks = this.state.books.filter(book =>
         book.title.toLowerCase().includes(searchQuery.toLowerCase())
       );
       this.setState({ books: filteredBooks });
-      console.log(filteredBooks)
+      console.log(filteredBooks);
     } else {
       this.setState({ books: Fantasy });
     }
   };
   render() {
     return (
-      
       <Container>
         <InputGroup>
-            <DropdownButton
-              as={InputGroup.Prepend}
-              id="dropdown-basic-button"
-              className="mb-3"
-              title={this.state.categorySelected}
-            >
-              {bookCategories.map((category, index) => {
-                return (
-                  <Dropdown.Item
-                    href="#/action-1"
-                    key={`dropdown-category-${index}`}
-                    onClick={() => this.handleDropdownChange(category)}
-                  >
-                    {category}
-                  </Dropdown.Item>
-                );
-              })}
-            </DropdownButton>
-            <FormControl
-              placeholder="Search Books by Title"
-              aria-label="Search"
-              aria-describedby="basic-addon1"
-              onChange={(e) => this.handleSearchQuery(e.target.value)}
-            />
-          </InputGroup>
-
-          <Row className="mt-4">
-            {this.state.books ? (this.state.books.map((item) => { 
-                
-                    return (
-                        <SingleBook title = {item.title} image ={item.img} key={item.asin} category={item.category} price={item.price}/>
-                        );
-                    })
-                  ) : (
-                      
-                    <div> Nothing here </div>
-                  )}
-                </Row>
-        </Container>
-      
+          <DropdownButton
+            as={InputGroup.Prepend}
+            id="dropdown-basic-button"
+            className="mb-3"
+            title={this.state.categorySelected}
+          >
+            {bookCategories.map((category, index) => {
+              return (
+                <Dropdown.Item
+                  href=""
+                  key={`dropdown-category-${index}`}
+                  onClick={() => this.handleDropdownChange(category)}
+                >
+                  {category}
+                </Dropdown.Item>
+              );
+            })}
+          </DropdownButton>
+          <FormControl
+            placeholder="Search Books by Title"
+            aria-label="Search"
+            aria-describedby="basic-addon1"
+            onChange={e => this.handleSearchQuery(e.target.value)}
+          />
+        </InputGroup>
+        <h1>{this.state.categorySelected}</h1>
+        {/* <div>{this.state.books.map((book) => book.title)}</div> */}
+        <Row className="mt-4">
+          {this.state.books ? (
+            this.state.books.map(item => {
+              return (
+                <Col xs={1} md={3} lg={2}>
+                  <Card className="card, mb-2">
+                    <div className="card-img ">
+                      <img
+                        className="card-img-top"
+                        src={item.img}
+                        alt={item.title}
+                        onClick={this.selectBook}
+                        style={{ height: "200px", objectFit: "cover" }}
+                      />
+                    </div>
+                    <div className="card-body">
+                      <MyBadge category={item.category} />
+                      <h6 className="bookTitle">{item.title}</h6>
+                      <Button className="btn-warning">€{item.price}</Button>
+                    </div>
+                  </Card>
+                </Col>
+              );
+            })
+          ) : (
+            <div> Nothing here </div>
+          )}
+        </Row>
+      </Container>
     );
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,65 +1,37 @@
 const {
-    REACT_APP_PROFILE,
-    REACT_APP_TOKEN,
-    REACT_APP_PROFILELIST,
-    REACT_APP_POSTS,
-  } = process.env;
+  REACT_APP_PROFILE,
+  REACT_APP_TOKEN,
+  REACT_APP_PROFILELIST,
+  REACT_APP_POSTS,
+} = process.env;
 
 export const fetchBooks = async () => {
-    try {
-      const res = await fetch("http://localhost:3006/books", {
-        headers: {
-          Authorization: "Bearer " ,
-        },
-      });
-      const books= await res.json();
-      if (res.ok) {
-        console.log("all good");
-        return books;
-      } else {
-        console.log("there is an error with books");
-      }
-    } catch (err) {
-      console.log("there is an error with fetching books");
+  try {
+    const res = await fetch(`${process.env.REACT_APP_BE_URL}/books` );
+    const books = await res.json();
+    if (res.ok) {
+      console.log("all good");
+      return books;
+    } else {
+      console.log("there is an error with books");
     }
-  };
+  } catch (err) {
+    console.log("there is an error with fetching books");
+  }
+};
 
-
-
-  export const fetchComments = async (asin) => {
-    try {
-      const res = await fetch(`http://localhost:3006/books/${asin}/comments`, {
-        headers: {
-          Authorization: "Bearer " ,
-        },
-      });
-      if (res.ok) {
-         const export const fetchComments = async (asin) => {
-            try {
-              const res = await fetch(`http://localhost:3006/books/${asin}/comments`, {
-                headers: {
-                  Authorization: "Bearer " + REACT_APP_TOKEN,
-                },
-              });
-              if (res.ok) {
-                 const comments = await res.json();
-                 console.log(comments)
-                 return comments;
-              } else {
-                console.log('there is an error with fetching comments')
-              }
-             
-            } catch (err) {
-              console.log("there is an error");
-            }
-          }; = await res.json();
-         console.log(comments)
-         return comments;
-      } else {
-        console.log('there is an error with fetching comments')
-      }
-     
-    } catch (err) {
-      console.log("there is an error");
+export const fetchComments = async asin => {
+  try {
+    const res = await fetch(
+      `${process.env.REACT_APP_BE_URL}/books/${asin}/comments` );
+    if (res.ok) {
+      const comments = await res.json();
+      console.log(comments);
+      return comments;
+    } else {
+      console.log("there is an error with fetching comments");
     }
-  };
+  } catch (err) {
+    console.log("there is an error");
+  }
+};
